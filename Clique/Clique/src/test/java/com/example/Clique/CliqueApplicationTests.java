670a0406b11c31697f3cfcec4f28ecf3c67ffe0e
Jeremy Ascher <jaasch54@gmail.com>
2024-12-27 14:08:09 -0500

    first commit of clique application

---
+++ Clique/Clique/src/test/java/com/example/Clique/CliqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Clique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
