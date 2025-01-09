a2b6badb689d09506c92d40b0a4263119d322c4c
Matt <84721683+mattry@users.noreply.github.com>
2025-01-09 15:17:36 -0500

    Add Route for follow requests

--- clique-frontend/src/App.js
+++ clique-frontend/src/App.js
@@ -6,6 +6,7 @@ import LandingPage from './components/LandingPage';
 import UserProfileComponent from './components/profile-components/UserProfileComponent';
 import SearchBar from './components/search-components/SearchBar';
 import Feed from './components/post-components/Feed';
+import FollowRequestComponent from './components/follow-request-components/FollowRequestComponent';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Route path="/user/:id" element={<UserProfileComponent />} />
             <Route path="/search" element={<SearchBar />} />
             <Route path="/explore" element={<Feed explore={true}/>} />
+            <Route path="/requests" element={<FollowRequestComponent />} />
           </Routes>
         </Router>
     </UserProvider>
