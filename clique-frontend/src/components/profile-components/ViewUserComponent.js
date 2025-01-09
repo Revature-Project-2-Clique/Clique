c03768f9447e613a6400b533d622ae25bc7d8568
Matt <84721683+mattry@users.noreply.github.com>
2025-01-09 13:56:57 -0500

    Edit view user and connection management to handle request to follow on private accounts

--- clique-frontend/src/components/profile-components/ViewUserComponent.js
+++ clique-frontend/src/components/profile-components/ViewUserComponent.js
@@ -1,15 +1,36 @@
 import ConnectionDisplay from "./ConnectionDisplay";
 import ConnectionManagement from "./ConnectionManagement";
 import PostList from "../post-components/PostList";
+import { useEffect, useState } from "react";
+import { useUser } from "../UserContext";
+import api from "../../service/api";
 
 const ViewUserComponent = ({displayUser, posts, followers, following, getFollowers, getFollowing, setPosts}) => {
+
+    const { user, token } = useUser();
+    const [connection, setConnection] = useState(null);
+
+    const headers = token ? { Authorization: `Bearer ${token}`, "Content-Type": "application/json" } : {};
+
+    const getConnectionStatus = async () => {
+        try {
+            const response = await api.get(`/connection/${user.userId}/isFollowing/${displayUser.userId}`, { headers });
+            setConnection(response.data);
+        } catch (error) {
+            console.error("Error getting connection status: ", error)
+        }
+    };
+
+    useEffect(()=>{
+        getConnectionStatus();
+    },[]);
 
     return(
         <>
             <h2>{displayUser.username}</h2>
             <h3>{displayUser.firstName} {displayUser.lastName}</h3>
-            <ConnectionDisplay followers={followers} following={following} />
-            <ConnectionManagement displayUser={displayUser} getFollowers={getFollowers} getFollowing={getFollowing} /><br/>
+            <ConnectionDisplay followers={followers} following={following} /><br/>
+            <ConnectionManagement displayUser={displayUser} getFollowers={getFollowers} getFollowing={getFollowing} connection={connection} setConnection={setConnection} /><br/>
             <PostList posts={posts} setPosts={setPosts} />
         </>
     )
