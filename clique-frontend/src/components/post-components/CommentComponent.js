6a8444a26cc8707ac2af9fdba381960d9209a203
smistry4 <smistry1@vols.utk.edu>
2025-01-10 16:49:19 -0500

    added delete and update comment

--- clique-frontend/src/components/post-components/CommentComponent.js
+++ clique-frontend/src/components/post-components/CommentComponent.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import { useState } from 'react';
 
-const CommentComponent = ({comment}) => {
+const CommentComponent = ({comment, onDelete, onEdit}) => {
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [editText, setEditText] = useState(comment.comment_text);
+
+  const handleSaveEdit = () => {
+    if (editText.trim()) {
+      onEdit(editText);
+      setIsEditing(false);
+    }
+  };
+
   return (
     <div>
         <p><strong>{comment.username}</strong></p>
         <p><small>{new Date(comment.posted_Time).toLocaleString()}</small></p>
-        <p>{comment.comment_text}</p>
+        {isEditing ? (
+          <div>
+            <input
+              type="text"
+              value={editText}
+              onChange={(e) => setEditText(e.target.value)}
+            />
+            <button onClick={handleSaveEdit}>Save</button>
+            <button onClick={() => setIsEditing(false)}>Cancel</button>
+          </div>
+        ) : (
+          <p>{comment.comment_text}</p>
+        )}
+        {!isEditing ? onEdit && (
+        <>
+          <button onClick={() => setIsEditing(true)}>Edit</button>
+          <button onClick={() => onDelete(comment.comment_id)}>Delete</button>
+        </>
+      ): <></>}
     </div>
   )
 }
--- clique-frontend/src/components/post-components/CommentList.js
+++ clique-frontend/src/components/post-components/CommentList.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import { useUser } from '../UserContext'
 import CommentComponent from './CommentComponent'
 import CommentInput from './CommentInput'
 
-const CommentList = ({comments, onAddComment}) => {
+const CommentList = ({comments, onAddComment, onDeleteComment, onEditComment}) => {
+  const {user} = useUser();
   return (
     <>
         {comments && comments.length > 0 && (
             <div >
                 <h6>Comments:</h6>
                 {Array.from(comments).map((comment) => (
-                    <CommentComponent key={comment.comment_id} comment={comment} />
+                    <CommentComponent 
+                      key={comment.comment_id} 
+                      comment={comment} 
+                      onDelete={comment.username === user.username ? () => onDeleteComment(comment.comment_id) : null}
+                      onEdit={comment.username === user.username ? (newText) => {onEditComment(comment.comment_id, newText)} : null}
+                    />
                 ))}
             </div>
         )}
