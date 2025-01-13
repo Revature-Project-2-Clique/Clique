2a638c69e9ef59d0f017bad975773fdebecd364b
Matt <84721683+mattry@users.noreply.github.com>
2025-01-13 14:30:08 -0500

    Style follow request list

--- clique-frontend/src/components/follow-request-components/DisplayRequests.js
+++ clique-frontend/src/components/follow-request-components/DisplayRequests.js
@@ -1,14 +1,25 @@
+import { Link } from "react-router-dom";
+
 const DisplayRequests = ({ requests, approveHandler, denyHandler }) => {
 
     return(
-        <>
+        <div className="max-w-screen-xl mx-auto p-6 bg-white shadow-md rounded-md space-y-6 flex items-center justify-center">
             {
                 requests.length > 0 ? (
                     requests.map((request) =>
                     <div key={request.requestId}>
-                        <span>{request.requester.username} wants to follow you.</span>
-                        <button onClick={()=>approveHandler(request.requesterId)}>Approve</button>
-                        <button onClick={()=>denyHandler(request.requesterId)}>Deny</button>
+                        <Link to={`/user/${request.requesterId}`} className="text-xl font-bold text-[#003a92] hover:underline">
+                            {request.requester.username}    
+                        </Link>
+                        <span className="text-xl font-bold text-[#78716c]"> wants to follow you:</span>
+                        <span 
+                            onClick={()=>approveHandler(request.requesterId)}
+                            className="cursor-pointer pl-2  text-[#003a92] hover:underline"
+                        >Approve</span>
+                        <span 
+                            onClick={()=>denyHandler(request.requesterId)}
+                            className="cursor-pointer pl-2  text-[#003a92] hover:underline"
+                        >Deny</span>
                         <br/>
                     </div>
                     )
@@ -16,7 +27,7 @@ const DisplayRequests = ({ requests, approveHandler, denyHandler }) => {
                     <p>No pending follow requests.</p>
                 )
             }
-        </>
+        </div>
     )
 
 }
